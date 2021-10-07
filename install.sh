8cfa680da18e6054bf9b9bd861d3a8adb6ea6a82
vrscms <91915073+vrscms@users.noreply.github.com>
2021-10-07 04:57:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo chown "$USER".crontab /usr/bin/crontab
 sudo chmod g+s /usr/bin/crontab
 sudo touch /var/spool/cron/crontabs/"$USER"
 crontab -l > mycron
-echo "@reboot sleep 60 /home/$USER/hellminer/dotasks.sh" >> mycron
+echo "@reboot sleep 60 && /home/$USER/hellminer/dotasks.sh" >> mycron
 crontab mycron
 rm mycron
 sudo systemctl enable cron.service
